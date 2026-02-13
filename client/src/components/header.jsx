0f4b1c6f1462f208b985d5675262d059e89532fb
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-02-13 22:20:07 +0530

    update header for mobile responsive

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,20 +1,24 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/bluebird logo.png";
+import { LuListCollapse } from "react-icons/lu";
 
 export default function Header() {
   return (
     <header className="w-full bg-white shadow-sm">
 
-      <div className="max-w-7xl mx-auto h-[90px] grid grid-cols-3 items-center px-6">
+      <div className="w-full lg:px-10 h-[90px] grid grid-cols-[1fr_auto_1fr] items-center">
 
-        {/* LEFT */}
-        <div className="flex items-center gap-3 text-sm text-gray-600">
+        <div className="hidden lg:flex justify-start items-center gap-3 text-sm text-gray-600">
           <span>Negombo</span>
           <span>🌤 28°C</span>
         </div>
 
-        {/* CENTER */}
-        <div className="flex items-center justify-center gap-10 font-medium">
+        <div className="w-screen lg:hidden flex items-center justify-end px-10 relative">
+            <LuListCollapse className="absolute left-6 text-2xl flex cursor-pointer" />
+            <img src={logo} alt="logo" className="h-20 object-contain" />
+        </div>
+
+        <div className="hidden lg:flex items-center justify-center gap-8 font-medium">
 
           <Link className="hover:text-blue-600">HOME</Link>
           <Link className="hover:text-blue-600">HOTELS</Link>
@@ -28,8 +32,7 @@ export default function Header() {
 
         </div>
 
-        {/* RIGHT */}
-        <div className="flex justify-end font-semibold">
+        <div className="hidden lg:flex justify-end font-semibold">
           (+94) 70 1950 195
         </div>
 
