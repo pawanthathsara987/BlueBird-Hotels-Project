d388a9c550f5b011fe1aa0f58e4076b7ba1f81b1
sandeepal513 <sandeepal513@gmail.com>
2026-02-15 22:57:00 +0530

    create ui of bookingprocessing.jsx including guest information, payment details and review and confirm payment and create route for bookingProcessing.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import BookingRoom from "./pages/client/booking/roomBooking";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homePage";
+import BookingProcess from "./pages/client/booking/bookingProcessing";
+import BookingProcessing from "./pages/client/booking/bookingProcessing";
+import Process from "./pages/client/booking/procee";
 
 export default function App() {
     return(
@@ -9,6 +12,7 @@ export default function App() {
                 <Routes>
                     <Route path="/*" element={<HomePage/>} />
                     <Route path="/booking" element={<BookingRoom />} />
+                    <Route path="/booking/process" element={<BookingProcessing />} />
                 </Routes>
             </div>
         </BrowserRouter>
