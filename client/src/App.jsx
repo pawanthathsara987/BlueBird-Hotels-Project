404ec3ac5c964a9a02c49a42d14cbccf247c8b66
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-02-18 15:28:52 +0530

    Login Route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import BookingRoom from "./pages/client/booking/roomBooking";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import BookingProcessing from "./pages/client/booking/bookingProcessing";
+import LoginAdmin from "./pages/admin/loginAdmin";
 
 export default function App() {
     return(
@@ -11,6 +12,7 @@ export default function App() {
                     <Route path="/*" element={<HomePage/>} />
                     <Route path="/booking" element={<BookingRoom />} />
                     <Route path="/booking/process" element={<BookingProcessing />} />
+                    <Route path="/loginAdmin" element={<LoginAdmin />} />
                 </Routes>
             </div>
         </BrowserRouter>
