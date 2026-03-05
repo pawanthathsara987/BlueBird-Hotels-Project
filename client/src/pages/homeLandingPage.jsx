4f2fc654b42a5faa311521ac18ecd0f4b1e8aff5
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-03-05 20:41:23 +0530

    style: update BookingNavigation component and adjust layout for HomeLandingPage

--- client/src/pages/homeLandingPage.jsx
+++ client/src/pages/homeLandingPage.jsx
@@ -6,10 +6,10 @@ export default function HomeLandingPage() {
         <div className="w-full">
             <div className="relative w-full">
                 <ImageCarousel />
-                <BookingNavigation />
+                <BookingNavigation className="" />
             </div>
-            <div className="w-full h-75 mt-20 px-10 flex items-center justify-center flex-col gap-5">
-                <h2 className="text-3xl font-bold text-black/30 text-center mt-10">Welcome to BlueBird Hotel</h2>
+            <div className="w-full h-75 mt-65 sm:mt-40  lg:mt-50 px-10 flex items-center justify-center flex-col gap-5">
+                <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-black/30 text-center mt-10\">Welcome to BlueBird Hotel</h2>
                 <div className="w-150 ">
                     <p className="text-center mt-4 text-gray-600 leading-7.5">Blue Bird Hotels is the right choice for visitors
                     who are searching for a combination of charm and a convenient position where to explore the
