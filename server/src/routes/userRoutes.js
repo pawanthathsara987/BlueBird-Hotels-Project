3aabaaaef7cd4dc7271febe81d178f0e85ed1300
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-03-09 01:26:09 +0530

    add user routes for CRUD operations on users

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import {registerUser} from "../controllers/userController.js";
+import {registerUser, getAllUsers, updateUser, deleteUser} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/create", registerUser);
+userRouter.get("/getAll", getAllUsers);
+userRouter.put("/update/:id", updateUser);
+userRouter.delete("/delete/:id", deleteUser);
 
 export default userRouter;
