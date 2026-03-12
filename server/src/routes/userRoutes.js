a52112f80787c822bcfc6e24f6041c496527abea
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-03-12 21:56:17 +0530

    add staff member registration route to user routes

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {registerUser, getAllUsers, updateUser, deleteUser, searchUsers, verifyEmail} from "../controllers/userController.js";
+import {registerUser, getAllUsers, updateUser, deleteUser, searchUsers, verifyEmail, registerStaffMember} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
@@ -9,5 +9,6 @@ userRouter.get("/getAll", getAllUsers);
 userRouter.put("/update/:id", updateUser);
 userRouter.delete("/delete/:id", deleteUser);
 userRouter.post("/verify-email", verifyEmail);
+userRouter.post("/registerStaffMember", registerStaffMember);
 
 export default userRouter;
