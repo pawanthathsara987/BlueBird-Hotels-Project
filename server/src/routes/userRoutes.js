263739e2f997e8a89519e32383d4f98aa52e48c5
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-03-13 22:55:24 +0530

    add send OTP route to user routes

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {registerUser, getAllUsers, updateUser, deleteUser, searchUsers, verifyEmail, registerStaffMember} from "../controllers/userController.js";
+import {registerUser, getAllUsers, updateUser, deleteUser, searchUsers, verifyEmail, registerStaffMember, sendOtp} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
@@ -10,5 +10,6 @@ userRouter.put("/update/:id", updateUser);
 userRouter.delete("/delete/:id", deleteUser);
 userRouter.post("/verify-email", verifyEmail);
 userRouter.post("/registerStaffMember", registerStaffMember);
+userRouter.post("/send-otp", sendOtp);
 
 export default userRouter;
