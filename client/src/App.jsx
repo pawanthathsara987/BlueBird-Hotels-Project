fbc3cda34d46ca0347b31d6c00fc2f56239808fd
Kumudu Amarakoon <kumudu4746@gmail.com>
2026-03-28 14:29:33 +0530

    add tour manager page route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,10 @@ import LoginAdmin from "./pages/admin/loginAdmin";
 import LoginReceptionist from "./pages/reception/loginReceptionist";
 import UserRegister from "./pages/userRegister";
 import BookingTour from "./pages/client/booking/tourBooking";
-import AddTour from "./pages/admin/TourForm";
 import AdminPage from "./pages/admin/AdminPage";
 import PasswordResetPage from "./pages/reception/PasswordResetPage";
 import { Toaster } from "react-hot-toast";
+import ManagerPage from "./pages/manager/managerPage";
 
 export default function App() {
     return(
@@ -23,10 +23,10 @@ export default function App() {
                     <Route path="/loginAdmin" element={<LoginAdmin />} />
                     <Route path="/loginReceptionist" element={<LoginReceptionist />} />
                     <Route path="/userRegister" element={<UserRegister />} />
-                    <Route path="/booking/tour" element={< BookingTour />} />
-                    <Route path="/addTour" element={< AddTour />} />
+                    <Route path="/booking/tour" element={< BookingTour />} />           
                     <Route path="/reset-password" element={< PasswordResetPage />} />
                     <Route path="/admin/*" element={< AdminPage />} />
+                    <Route path="/manager/*" element={< ManagerPage />} />
                 </Routes>
             </div>
         </BrowserRouter>
