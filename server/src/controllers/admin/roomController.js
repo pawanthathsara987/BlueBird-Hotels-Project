cda7cd0668531bc156f30f176307374487fef78e
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-04-22 12:17:07 +0530

    add initial implementation of addRoom function in roomController

---
+++ server/src/controllers/admin/roomController.js
@@ -0,0 +1,11 @@
+import roomModel from "../../models/roomModel.js";
+import roomAmenities from "../../models/roomAmenities.js";
+import packageModel from "../../models/packageModel.js";
+
+export async function addRoom(req, res) {
+
+    try{
+       // const {roomNumber, roomStatus, packageId, amenities} = req.body;
+    }
+
+}
