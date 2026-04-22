8c1c007f63c60fdad242cf260331ebba5532037d
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-04-22 12:55:02 +0530

    refactor addRoom function to improve error handling and response structure

--- server/src/controllers/admin/roomController.js
+++ server/src/controllers/admin/roomController.js
@@ -4,8 +4,36 @@ import packageModel from "../../models/packageModel.js";
 
 export async function addRoom(req, res) {
 
-    try{
-       // const {roomNumber, roomStatus, packageId, amenities} = req.body;
+    try {
+        const { roomNo, status, packageId, amenities } = req.body;
+
+        if (!roomNo || !status || !packageId) {
+            return res.status(400).json({
+                success: false,
+                message: "Missing required fields"
+            });
+        }
+
+        const newRoom = await roomModel.create({
+            roomNo,
+            status,
+            packageId,
+            amenities,
+        });
+
+        return res.status(201).json({
+            success: true,
+            message: "Room created successfully",
+            data: newRoom
+        });
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Failed to create room",
+            error: error.message
+        });
     }
 
 }
