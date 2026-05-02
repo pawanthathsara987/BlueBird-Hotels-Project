15a6756d55c7463d07129bbcaf00bfcd5fe36a2d
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-05-02 23:49:10 +0530

    feat: add Customer model with validation and attributes

---
+++ server/src/models/User/Customer.js
@@ -0,0 +1,46 @@
+import {Model, DataTypes} from 'sequelize';
+import sequelize from '../../config/database.js';
+
+class Customer extends Model { }
+
+Customer.init(
+    {
+        customerId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        firstName: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+        phoneNumber: {
+            type: DataTypes.STRING(20),
+            allowNull: true
+        }
+    },
+    {
+        sequelize,
+        modelName: 'Customer',
+        tableName: 'customer',
+        timestamps: true
+    }
+);
+
+export default Customer;
