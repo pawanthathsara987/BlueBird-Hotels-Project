fd0c372940b4bc73e926761ad81beb2063069f19
Pawan Thathsara <pawanthathsara987@gmail.com>
2026-05-06 01:13:35 +0530

    feat: add routes for OTP sending and password reset functionality

--- server/src/routes/customerRoutes.js
+++ server/src/routes/customerRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { registerCustomer, loginCustomer } from "../controllers/customerController.js";
+import { registerCustomer, loginCustomer, sendOTP, verifyOTPAndResetPassword } from "../controllers/customerController.js";
 
 const customerRouter = express.Router();
 
 customerRouter.post("/register", registerCustomer);
 customerRouter.post("/login", loginCustomer);
+customerRouter.post("/send-otp", sendOTP);
+customerRouter.post("/reset-password", verifyOTPAndResetPassword);
+
 
 export default customerRouter;
