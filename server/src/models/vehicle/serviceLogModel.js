b6e0bebadeae557bc9486e8652d3b85ec44996a3
Kumudu Amarakoon <kumudu4746@gmail.com>
2026-05-25 18:20:06 +0530

    fix: remove

--- server/src/models/vehicle/serviceLogModel.js
+++
@@ -1,6 +0,0 @@
-import VehicleServiceLog from './vehicleServiceLogModel.js';
-
-// Legacy compatibility alias.
-// The active vehicle service log model lives in vehicleServiceLogModel.js.
-export default VehicleServiceLog;
-export { VehicleServiceLog };
