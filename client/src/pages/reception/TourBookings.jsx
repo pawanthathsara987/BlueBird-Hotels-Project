a024bc5d7c724fa4f340d2133084762f0702f4dc
Kumudu Amarakoon <kumudu4746@gmail.com>
2026-07-04 13:17:04 +0530

    feat: update pickup location to 'BlueBird Hotel Lobby' in TourInquiryPage

--- client/src/pages/reception/TourBookings.jsx
+++ client/src/pages/reception/TourBookings.jsx
@@ -62,7 +62,7 @@ export default function TourBookings() {
         numberOfAdults: 1,
         numberOfChildren: 0,
         startDate: getMinStartDate(),
-        pickupLocation: "Hotel Lobby",
+        pickupLocation: "BlueBird Hotel Lobby",
         specialRequests: ""
     });
 
