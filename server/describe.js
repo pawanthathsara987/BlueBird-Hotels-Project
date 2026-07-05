82d2ffb0830beaadc2374b05a75db82563c8b00f
Kumudu Amarakoon <kumudu4746@gmail.com>
2026-07-05 09:58:43 +0530

    refactor: replace direct database query with vehicle rental policy controller logic in describe.js

--- server/describe.js
+++ server/describe.js
@@ -1,15 +1,1 @@
-import 'dotenv/config';
-import sequelize from './src/config/database.js';
-import { QueryTypes } from 'sequelize';
-
-async function run() {
-  try {
-    const rows = await sequelize.query("SELECT * FROM vehicle_rental_policies WHERE id = 1", { type: QueryTypes.SELECT });
-    console.log("Current row in DB:", rows[0]);
-  } catch (err) {
-    console.error("Error:", err);
-  } finally {
-    process.exit(0);
-  }
-}
-run();
+// Temporary test script
